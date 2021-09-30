09ba0a45cd92996035f1a7e21758943a3ef6dcf1
timds <timds_pub@icloud.com>
2021-09-30 10:24:18 +0000

    Upgrade version of libxlsxwriter dependency
    To get the use_dtoa_library flag (40-50% faster double-to-string
    conversion)

--- default.nix
+++ default.nix
@@ -5,7 +5,15 @@ let
   pkgs = import (builtins.fetchTarball url) {
     config = {
       packageOverrides = super: {
-        libxlsxwriter = super.libxlsxwriter.overrideAttrs (oldAttrs: {
+        libxlsxwriter = super.libxlsxwriter.overrideAttrs (oldAttrs: rec {
+          # use newer version to get dtoa flag
+          version = "1.1.3";
+          src = pkgs.fetchFromGitHub {
+            owner = "jmcnamara";
+            repo = "libxlsxwriter";
+            rev = "RELEASE_${version}";
+            sha256 = "0b97wq0f9w30aamxi641b6srr59i45vkxyx8c1v1yxq59yb6kswg";
+          };
           # use_dtoa_library: for 40-50% faster double-to-string conversion
           makeFlags = oldAttrs.makeFlags ++ ["USE_DTOA_LIBRARY=1"];
         });
