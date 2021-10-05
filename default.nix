c7fd83f2fd1014d342c07e8a1d5c922c558f4cb3
timds <timds_pub@icloud.com>
2021-10-05 14:15:44 +0000

    Parameterise nixpkgs

--- default.nix
+++ default.nix
@@ -1,34 +1,18 @@
-let
-  rev = "07ca3a021f05d6ff46bbd03c418b418abb781279"; # first 21.05 release
-  url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-  compiler = "ghc8104";
-  pkgs = import (builtins.fetchTarball url) {
-    config = {
-      packageOverrides = super: {
-        libxlsxwriter = super.libxlsxwriter.overrideAttrs (oldAttrs: rec {
-          # use newer version to get dtoa flag
-          version = "1.1.3";
-          src = pkgs.fetchFromGitHub {
-            owner = "jmcnamara";
-            repo = "libxlsxwriter";
-            rev = "RELEASE_${version}";
-            sha256 = "0b97wq0f9w30aamxi641b6srr59i45vkxyx8c1v1yxq59yb6kswg";
-          };
-          # use_dtoa_library: for 40-50% faster double-to-string conversion
-          makeFlags = oldAttrs.makeFlags ++ ["USE_DTOA_LIBRARY=1"];
-        });
-      };
-    };
-  };
+{ pkgs ? import ./nixpkgs.nix,
+  compiler ? "ghc8104"
+}:
 
+let
   hpkgs = pkgs.haskell.packages."${compiler}";
 in
 hpkgs.developPackage {
   root = ./.;
   withHoogle = false;
-  modifier = drv: with pkgs.haskell.lib; addExtraLibrary (addBuildTools drv [
-    pkgs.cabal-install
-    hpkgs.c2hs
-  ])
-  pkgs.libxlsxwriter;
+  modifier = drv: with pkgs.haskell.lib;
+    addExtraLibrary (
+      addBuildTools drv [
+        pkgs.cabal-install
+        hpkgs.c2hs
+      ]
+    ) pkgs.libxlsxwriter;
 }
